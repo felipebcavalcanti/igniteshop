dfa962cf7b9fee08846a6b830987fdfe9cb9238d
Felipe Cavalcanti <fb.cavalcanti1@gmail.com>
2023-01-17 15:51:05 -0300

    Criação da página do IgniteShop

---
+++ src/pages/index.tsx
@@ -0,0 +1,77 @@
+import { HomeContainer, Product } from "@/styles/pages/home";
+
+import Image from "next/image";
+
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css";
+
+import camiseta1 from "../assets/1.png";
+import camiseta2 from "../assets/2.png";
+import camiseta3 from "../assets/3.png";
+
+import { stripe } from "../lib/stripe";
+import { GetServerSideProps } from "next";
+import Stripe from "stripe";
+
+
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    price: number;
+    imageUrl: string;
+  }[]
+
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  });
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+
+     {products.map(product => {
+        return(
+          <Product key={product.id} className="keen-slider__slide" >
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product >
+        )
+     })}
+
+     </HomeContainer>
+    
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"]
+  })
+
+  const product = response.data.map(product => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount! / 100,
+    }
+  })
+
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
+}
